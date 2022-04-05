64583246f7aa69b707ee87261d8a58d106b67710
Alexander Valter <33101219+greatspoke@users.noreply.github.com>
2022-04-05 11:16:50 +0300

    Add component Tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Htag, Button, P } from '../components';
+import { Htag, Button, P, Tag } from '../components';
 
 export default function Home(): JSX.Element {
 
@@ -8,6 +8,9 @@ export default function Home(): JSX.Element {
       <Button appearance='primary' arrow='right' className='dsds'>Button</Button>
       <Button appearance='ghost' arrow='down'> Button ghost</Button>
       <P size='l'>Paragraph</P>
+      <Tag size='s' color='red'>Red</Tag>
+      <Tag size='s' color='green'>Green</Tag>
+      <Tag size='m' color='primary'>Primary</Tag>
     </>
   );
 }
