4e84722e1bca028b95c31d398225bdb09482c463
Alexander Valter <33101219+greatspoke@users.noreply.github.com>
2022-04-05 12:05:25 +0300

    Add layout components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 import { Htag, Button, P, Tag, Rating } from '../components';
-
+import { Layout } from "../layout/Layout"; 
 export default function Home(): JSX.Element {
 
   const [rating, setRating] = useState<number>(0);
 
   return (
-    <>
+    <Layout>
       <Htag tag='h1'>h1</Htag>
       <Button appearance='primary' arrow='right' className='dsds'>Button</Button>
       <Button appearance='ghost' arrow='down'> Button ghost</Button>
@@ -15,6 +15,6 @@ export default function Home(): JSX.Element {
       <Tag size='s' color='green'>Green</Tag>
       <Tag size='m' color='primary'>Primary</Tag>
       <Rating rating={rating} isEditable setRating={setRating}></Rating>
-    </>
+    </Layout>
   );
 }
