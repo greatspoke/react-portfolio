467325715ae2df244532d4b0b08a2c65a624de95
Alexander Valter <33101219+greatspoke@users.noreply.github.com>
2022-04-05 12:12:44 +0300

    Add HOC layout

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,7 +1,7 @@
 import { LayoutProps } from './Layout.props';
 import styles from './P.module.css';
 import cn from 'classnames';
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import { Header } from './Header/Header';
 import { Sidebar } from './Sidebar/Sidebar';
 import { Footer } from './Footer/Footer';
@@ -21,4 +21,14 @@ export const Layout = ({ children }: LayoutProps): JSX.Element => {
 		</>
 	)
 
+}
+
+export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
+	return function withLayoutComponent(props: T): JSX.Element {
+		return (
+			<Layout>
+				<Component {...props} />
+			</Layout>
+		)
+	}
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import { Htag, Button, P, Tag, Rating } from '../components';
-import { Layout } from "../layout/Layout"; 
-export default function Home(): JSX.Element {
+import { withLayout } from '../layout/Layout';
+
+function Home(): JSX.Element {
 
   const [rating, setRating] = useState<number>(0);
 
   return (
-    <Layout>
+    <>
       <Htag tag='h1'>h1</Htag>
       <Button appearance='primary' arrow='right' className='dsds'>Button</Button>
       <Button appearance='ghost' arrow='down'> Button ghost</Button>
@@ -15,6 +16,8 @@ export default function Home(): JSX.Element {
       <Tag size='s' color='green'>Green</Tag>
       <Tag size='m' color='primary'>Primary</Tag>
       <Rating rating={rating} isEditable setRating={setRating}></Rating>
-    </Layout>
+    </>
   );
 }
+
+export default withLayout(Home);
